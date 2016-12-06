4fbb32a2205745a9731b3bd1df2075b0bd6d2155
Javier Ocampo <javier.ocampo.contractor@weather.com>
2016-12-06 15:08:32 -0300

    created endpoints

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 'use strict'
 
 const express = require('express')
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = process.env.PORT || 3000
 
+// Midlewares
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.get('/api/product', (req, res) => {
+  res.status(200).send({products: []})
+})
+
+app.get('/api/product/:productId', (req, res) => {
+
+})
+
+app.post('/api/product', (req, res) => {
+  console.log(req.body)
+  res.status(200).send({message: 'the product its ok!'})
+})
+
+app.put('/api/product/:productId', (req, res) => {
+
+})
+
+app.delete('/api/product/:productId', (req, res) => {
+
+})
+
 app.listen(port, () => {
-    console.log(`API rest corriendo en http://localhost:${port}`)
+  console.log(`API rest corriendo en http://localhost:${port}`)
 })
